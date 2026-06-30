1839b1f635e0d20988af184bf4712780b3202aa7
Waltj73 <walt.seas@gmail.com>
2026-06-30 08:46:38 -0500

    Update __init__.py

--- ui/__init__.py
+++ ui/__init__.py
@@ -5,7 +5,21 @@ Taylor NQ UI Package
 """
 
 from .dashboard import Dashboard
+from .levels_panel import LevelsPanel
+from .price_panel import PricePanel
+from .signal_panel import SignalPanel
+from .sidebar import Sidebar
+from .statusbar import StatusBar
+from .table_panel import TablePanel
+from .theme import apply
 
 __all__ = [
     "Dashboard",
+    "LevelsPanel",
+    "PricePanel",
+    "SignalPanel",
+    "Sidebar",
+    "StatusBar",
+    "TablePanel",
+    "apply",
 ]
